ca3ad8bc1bef666e6af3d4a8853120937f7fb35e
Hvv <hvv7007@gmail.com>
2023-01-16 09:12:14 +0500

    fix

--- Deploy/provider.tf
+++ Deploy/provider.tf
@@ -15,8 +15,8 @@ provider "yandex" {
 
 
 locals {
-  token     = "y0_AgAAAAAjBSgkAATuwQAAAADZ3-t27FABVNeiRpmqq_2MkCe-wNnCGMU"
-  cloud_id  = "b1grumk416bgtrc0ufut"
-  folder_id = "b1gsb5ms5h345hauld53"
+  token     = ""
+  cloud_id  = ""
+  folder_id = ""
   zone      = "ru-central1-a"
 }
